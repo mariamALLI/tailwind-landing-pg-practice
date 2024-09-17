3ea1260f58c414d1662649d7cd0c82b24b87a05a
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2024-09-17 11:47:35 +0100

    initial commit
    practiced with tailwind using react.js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Maiin from "./Maiin"
+import Footer from "./Footer"
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    <Maiin />
+    <Footer />
+    </>
+  )
+}
+
+export default App
